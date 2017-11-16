4866d2e029e81dc3f49e4bdfee6e677f2dfd2b44
Romuald Brillout <github.com@brillout.com>
2017-11-17 00:03:37 +0100

    wip

--- core/server.js
+++ core/server.js
@@ -1,36 +1,45 @@
+const assert = require('reassert');
+const assert_usage = assert;
 const Repage = require('@repage/core/server');
 const renderToHtml = require('@repage/renderer-react/renderToHtml');
+const {initializeRepage} = require('./common');
 
 module.exports = {HapiReframe: HapiReframe()};
 
 function HapiReframe() {
     return {
+        name: 'reframe',
         register: (server, options) => {
             const {pages} = options;
-            assert_usage(pages);
+            assert_usage(pages, options);
 
             const repage = new Repage();
             initializeRepage({repage, pages, defaultPageInfo: {renderToHtml}});
 
             server.ext('onPreResponse', preResponse);
+
+            return;
+
+            async function preResponse(request, h) {
+                const {response} = request;
+                if( ! response.isBoom ) {
+                    return h.continue;
+                }
+
+                const uri = request.url.href;
+                assert(uri && uri.constructor===String, uri);
+
+                const html = await repage.getPageHtml({uri, canBeNull: true});
+                assert(html === null || html && html.constructor===String, html);
+
+                if( html === null ) {
+                    return h.continue;
+                }
+
+                const resp = h.response(html).type('text/html');
+                return resp;
+            }
         },
     };
 
-    function preResponse(request, h) {
-        const {response} = request;
-        if( ! response.isBoom ) {
-            return h.continue;
-        }
-
-        const uri = request.url.href;
-
-        const html = repage.getPageHtml({uri, canBeNull: true});
-
-        if( html === null ) {
-            return h.continue;
-        }
-
-        const resp = h.response(html).type('text/html');
-        return resp;
-    }
 }
--- example/flexible/server.js
+++ example/flexible/server.js
@@ -1,14 +1,14 @@
+process.on('unhandledRejection', err => {throw err});
 const {HapiReframe} = require('@reframe/core/server');
 const Hapi = require('hapi');
 const pages = require('./pages');
-
-const hReframe = new HapiReframe({pages});
 
 const server = Hapi.Server({port: 3000});
 
 (async () => {
     await server.register([{
-        plugin: hReframe,
+        plugin: HapiReframe,
+        options: {pages},
     }]);
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
