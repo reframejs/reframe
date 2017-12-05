90105915e3b97b96f1b9d2cda5e788f0f93cd2c5
Romuald Brillout <github.com@brillout.com>
2017-12-05 20:02:06 +0100

    wip

--- example/control/pages/index.js
+++ example/control/pages/index.js
@@ -43,3 +43,79 @@ module.exports = [
     //*/
     GameOfThronesCharacterPage,
 ];
+
+
+function RepagePageLoader() {
+    const pathname_to_page_id = {};
+    pathname_to_page_id['/game-of-thrones'] = 'GameOfThronesPage';
+
+    return {
+        _cObject: {
+            computedConfig: async () => {
+                if( page_data_is_locally_available() ) {
+                    await ensure_page_is_loaded();
+                    return null;
+                }
+                return {
+                    pageMixin: {
+                        routeObject: {
+                            doesMatchUrl,
+                        },
+                        renderToDom: {
+                            _cObject: {
+                                isDominant: true,
+                                computedValue: renderToDom__computedValue,
+                            },
+                        },
+                    },
+                };
+            },
+        },
+    };
+
+    function doesMatchUrl(url, {id}) {
+        const page_id = url.linkInfo['data-dynamic-link'] || pathname_to_page_id[url.pathname];
+        return page_id===id;
+    }
+
+    async function renderToDom__computedValue({overwrittenValues, currentObject, objectParts, addObjectPart, args, that}) {
+        const pageIsLoaded = overwrittenValues.length>0;
+        if( ! pageIsLoaded ) {
+            const pageInfo = await currentObject.pageLoader();
+            assert_usage(
+                pageInfo.renderToDom,
+                pageInfo,
+                "Loaded page information printed above is missing `renderToDom`",
+            );
+            addObjectPart(pageInfo);
+            assert_internal(overwrittenValues.length>0);
+        }
+        const renderToDom = overwrittenValues.slice(-1);
+        assert_internal(renderToDom);
+        renderToDom.apply(that, args);
+    }
+
+    async function ensure_page_is_loaded() {
+        if( ) {
+        }
+    }
+
+    function page_data_is_locally_available() {
+        if( typeof require === "undefined" ) {
+            return false;
+        }
+        if( ! require.resolve ) {
+            return false;
+        }
+        let fs_module_path;
+        try {
+            fs_module_path = require.resolve('fs');
+        } catch(e) {
+            return false;
+        }
+        if( ! fs_module_path ) {
+            return false;
+        }
+        return true;
+    }
+}
