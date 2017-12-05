934a2138de35fff1c0f436f9a383b70156ccb4e7
Romuald Brillout <github.com@brillout.com>
2017-12-05 12:53:47 +0100

    wip

--- example/control/pages/index.js
+++ example/control/pages/index.js
@@ -1,3 +1,4 @@
+/*
 const {LandingPage} = require('../../easy/pages/LandingPage');
 const {AboutPage} = require('../../easy/pages/AboutPage');
 const {GameOfThronesPage} = require('../../easy/pages/GameOfThronesPage');
@@ -7,5 +8,38 @@ module.exports = [
     LandingPage,
     AboutPage,
     GameOfThronesPage,
+    GameOfThronesCharacterPage,
+];
+*/
+const {LandingPage} = require('../../easy/pages/LandingPage');
+const {AboutPage} = require('../../easy/pages/AboutPage');
+//const {GameOfThronesPage} = require('../../easy/pages/GameOfThronesPage');
+const {GameOfThronesCharacterPage} = require('../../easy/pages/GameOfThronesCharacterPage');
+
+module.exports = [
+    LandingPage,
+    AboutPage,
+    //*
+    {
+        routeObject: {
+            doesMatchUrl(url) {
+                return url.pathname === '/game-of-thrones';
+            },
+            hasOnlyOneUniqueRoute() {
+                return true;
+            },
+            getRouteUri() {
+                return '/game-of-thrones';
+            },
+        },
+        id: 'GameOfThronesPage',
+        pageInfoLoader: async () => {
+            const ret = await import('../../easy/pages/GameOfThronesPage');
+            return ret.GameOfThronesPage;
+        },
+    },
+    /*/
+    GameOfThronesPage,
+    //*/
     GameOfThronesCharacterPage,
 ];
