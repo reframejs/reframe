670a48a0f918151703cfdd8538e8b15962644a63
Romuald Brillout <github.com@brillout.com>
2017-12-07 20:27:12 +0100

    wip

--- example/control/pages/index.js
+++ example/control/pages/index.js
@@ -13,13 +13,13 @@ module.exports = [
 */
 const {LandingPage} = require('../../easy/pages/LandingPage');
 const {AboutPage} = require('../../easy/pages/AboutPage');
-//const {GameOfThronesPage} = require('../../easy/pages/GameOfThronesPage');
+const {GameOfThronesPage} = require('../../easy/pages/GameOfThronesPage');
 const {GameOfThronesCharacterPage} = require('../../easy/pages/GameOfThronesCharacterPage');
 
 module.exports = [
     LandingPage,
     AboutPage,
-    //*
+    /*
     {
         routeObject: {
             doesMatchUrl(url) {
@@ -43,96 +43,3 @@ module.exports = [
     //*/
     GameOfThronesCharacterPage,
 ];
-
-
-function RepagePageLoader() {
-    return {
-        pageMixin: {
-            $meta: {
-                onInit: async (state, {addPart}) {
-                    const pageInfo = await state.pageLoader();
-                    addPart(pageInfo);
-                },
-                requires: ['pageLoader'],
-                isOptional: true,
-                props: {
-                    pageLoader: {
-                        isPublic: false,
-                    },
-                },
-            },
-        },
-    };
-}
-
-const pathname_to_page_name = {};
-pathname_to_page_name['/game-of-thrones'] = 'GameOfThronesPage';
-
-function RepagePageLoader() {
-    return {
-        pageMixin: {
-            routeObject: {doesMatchUrl},
-            renderToDom,
-            __page_has_loaded: false,
-            $meta: {
-                requires: ['pageLoader'],
-                isOptional: true,
-                isDisabled: ({__page_has_loaded}) => __page_has_loaded,
-                props: {
-                    routeObject: {
-                        doesMatchUrl: {
-                            isDominant: true,
-                        },
-                    },
-                    renderToDom: {
-                        isDominant: true,
-                    },
-                    pageLoader: {
-                        isPublic: false,
-                    },
-                },
-            },
-        },
-    };
-}
-
-async function renderToDom(state, {userArgs}) {
-    assert_internal(state.__page_has_loaded===false);
-    const pageInfo = await state.pageLoader();
-    assert_usage(
-        pageInfo.renderToDom,
-        pageInfo,
-        "Loaded page information printed above is missing `renderToDom`",
-    );
-    assert_internal(state.renderToDom===renderToDom);
-    addPart(pageInfo);
-    state.__page_has_loaded = true;
-    assert_internal(state.renderToDom!==renderToDom);
-    assert_internal(state.renderToDom===pageInfo.renderToDom);
-    return state.renderToDom.apply(null, userArgs);
-}
-
-function doesMatchUrl(url, {name}) {
-    const page_name = url.linkInfo['data-dynamic-link'] || pathname_to_page_name[url.pathname];
-    return page_name===name;
-}
-
-/*
-function page_data_is_locally_available() {
-    if( typeof require === "undefined" ) {
-        return false;
-    }
-    if( ! require.resolve ) {
-        return false;
-    }
-    let fs_module_path;
-    try {
-        fs_module_path = require.resolve('fs');
-    } catch(e) {
-        return false;
-    }
-    if( ! fs_module_path ) {
-        return false;
-    }
-    return true;
-}
