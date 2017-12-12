2c1ca093a46b6023fda465b007a7edeb75047016
Romuald Brillout <github.com@brillout.com>
2017-12-12 21:25:41 +0100

    wip

--- example/control/pages/index.js
+++ example/control/pages/index.js
@@ -15,10 +15,7 @@ const {LandingPage} = require('../../easy/pages/LandingPage');
 const {AboutPage} = require('../../easy/pages/AboutPage');
 const {GameOfThronesCharacterPage} = require('../../easy/pages/GameOfThronesCharacterPage');
 
-//require('../../easy/pages/GameOfThronesPage.js');
-//require.resolveWeak('../../easy/pages/GameOfThronesPage');
-//const req = require.context('../../easy/pages/', false, /\.js$/);
-const context = require.context('../../easy/pages', true);
+const context = require.context('../../easy/pages', false);
 
 module.exports = [
     LandingPage,
@@ -28,21 +25,14 @@ module.exports = [
         id: 'GameOfThronesPage',
         pageLoader: async () => {
          // const path = '../../easy/pages/GameOfThronesPage';
-            console.log(21);
-         // let res = context.resolve('./GameOfThronesPage');
-            console.log(22);
-          //console.log(res);
-            console.log(23);
-            console.log('before');
-         // const ret = await import(path);
-            const path = './GameOfThronesPage';
-            const ret = context(path);
-            console.log(ret);
-            console.log('after');
-         // throw 'eruihw';
          // const ret = await import('../../easy/pages/GameOfThronesPage');
-            console.log(ret);
-            return ret.GameOfThronesPage;
+         // const ret = await import(path);
+         // return ret.GameOfThronesPage;
+            {
+                const path = './GameOfThronesPage';
+                const ret = context(path);
+                return ret.GameOfThronesPage;
+            }
         },
     },
     /*/
