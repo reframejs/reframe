802366ae2b8e3b5873bdc4e40901879af4a883bc
Romuald Brillout <github.com@brillout.com>
2018-01-17 19:32:08 +0100

    wip

--- example/custom/browser/pages/custom-browser.html.js
+++ example/custom/browser/pages/custom-browser.html.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TimeComponent} from '../views/TimeComponent';
+import {TimeComponent} from '../../../views/TimeComponent';
 
 export default {
     route: '/custom-hydration',
