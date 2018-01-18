b22b8e8d510cde612cb80aae46bc2c20e1b3ee5b
Romuald Brillout <github.com@brillout.com>
2018-01-18 20:46:21 +0100

    wip

--- example/custom/express-server.js
+++ example/custom/express-server.js
@@ -5,7 +5,6 @@ const build = require('@reframe/build');
 
 const Repage = require('@repage/core');
 const {getPageHtml} = require('@repage/server');
-
 const RepageRouterCrossroads = require('@repage/router-crossroads');
 const RepageRenderer = require('@repage/renderer');
 const RepageRendererReact = require('@repage/renderer-react');
@@ -13,47 +12,35 @@ const RepageRendererReact = require('@repage/renderer-react');
 const path = require('path');
 const express = require('express');
 
-const pagesDir = path.resolve(__dirname, '../pages');
+runExpressServer();
 
-const app = express();
+async function runExpressServer() {
+    const pagesDir = path.resolve(__dirname, '../pages');
+    let pages;
+    const onBuild = args => {pages = args.pages};
+    const {browserDistPath} = (
+        await build({
+            pagesDir,
+            onBuild,
+        })
+    );
 
-let pages;
+    const app = express();
+    app.use(express.static(browserDistPath, {extensions: ['html']}));
+    app.get('*', async function(req, res, next) {
+        const {url} = req;
+        const html = await renderPageToHtml(url, pages);
 
-const onBuild = (
-    async args => {
-        assert_internal(args.pages);
-        /*
-        log(args);
-        log(Object.keys(args));
-        */
-        pages = args.pages;
-    }
-);
-
-app.get('*', async function(req, res, next) {
-    const {url} = req;
-    const html = await renderPageToHtml(url, pages);
-
-    if( ! html ) {
-        next();
-        return;
-    }
-    res.send(html);
-});
-
-(async () => {
-    const {browserDistPath} = await build({
-        pagesDir,
-        onBuild,
+        if( ! html ) {
+            next();
+            return;
+        }
+        res.send(html);
     });
-
-    app.use(express.static(browserDistPath));
-
     app.listen(3000, function () {
         console.log('App listening on port 3000');
     });
-})();
-
+}
 
 async function renderPageToHtml(uri, pages) {
     const repage = createRepageObject(pages);
