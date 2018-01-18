af11d55af633c23f1068dc825bcfe92f986fcdf0
Romuald Brillout <github.com@brillout.com>
2018-01-18 17:59:18 +0100

    wip

--- example/custom/server.js
+++ example/custom/server.js
@@ -7,25 +7,6 @@ const path = require('path');
 (async () => {
     const server = Hapi.Server({port: 3000});
 
-    addCustomRoute(server);
-
-    await addReframePlugins(server);
-
-    await server.start();
-    console.log(`Server running at ${server.info.uri}`);
-})();
-
-function addCustomRoute(server) {
-    server.route({
-        method: 'GET',
-        path:'/custom-route',
-        handler: function (request, h) {
-            return 'This is a custom route. This could for example be an API endpoint.'
-        }
-    });
-}
-
-async function addReframePlugins(server) {
     const {HapiServerRendering, HapiServeBrowserAssets} = (
         await getReframeHapiPlugins({
             pagesDir: path.resolve(__dirname, '../pages'),
@@ -37,4 +18,15 @@ async function addReframePlugins(server) {
         {plugin: HapiServeBrowserAssets},
         {plugin: HapiServerRendering},
     ]);
-}
+
+    server.route({
+        method: 'GET',
+        path:'/custom-route',
+        handler: function (request, h) {
+            return 'This is a custom route. This could for example be an API endpoint.'
+        }
+    });
+
+    await server.start();
+    console.log(`Server running at ${server.info.uri}`);
+})();
