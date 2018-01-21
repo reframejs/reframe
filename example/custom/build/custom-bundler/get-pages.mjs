9a7fef0ad030392e73570ea304bb46810909e6cd
Romuald Brillout <github.com@brillout.com>
2018-01-21 17:56:47 +0100

    wip

--- example/custom/build/custom-bundler/get-pages.mjs
+++ example/custom/build/custom-bundler/get-pages.mjs
@@ -1,9 +1,12 @@
 import HelloPage from './pages/hello.html.mjs';
 import LandingPage from './pages/landing.html.mjs';
+import expose from './expose.js';
 
 export default getPages;
 
 function getPages() {
+    const {__dirname} = expose;
+
     const pages = (
         [
             {pageObject: LandingPage, pageName: 'landing'},
@@ -12,7 +15,7 @@ function getPages() {
         .map(({pageObject, pageName}) => {
             const scripts = pageObject.scripts || [];
             scripts.push({
-                diskPath: './pages/'+pageName+'.entry.js',
+                diskPath: __dirname+'/pages/'+pageName+'.entry.js',
                 src: '/'+pageName+'-bundle.js',
                 bundleName: pageName+'Bundle',
                 _options: {skipAttributes: ['diskPath', 'bundleName']},
