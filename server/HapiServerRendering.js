d09323e27d6cf6d714bbac54e47832468e31be33
Romuald Brillout <github.com@brillout.com>
2018-02-21 19:38:18 +0100

    make code more readable

--- server/HapiServerRendering.js
+++ server/HapiServerRendering.js
@@ -3,17 +3,13 @@ const assert_usage = assert;
 const assert_internal = assert;
 const {compute_source_code_hash} = require('./utils/compute_source_code_hash');
 
-const {processReframeConfig} = require('@reframe/utils');
-
-const Repage = require('@repage/core');
 const {getPageHtml} = require('@repage/server');
 
 
-module.exports = {HapiServerRendering__create};
+module.exports = {HapiPlugin_ServerRendering__create};
 
 
-function HapiServerRendering__create({getPages, reframeConfig={}}={}) {
-    const repage_plugins = get_repage_plugins(reframeConfig);
+function HapiPlugin_ServerRendering__create(getRepageObject) {
 
     return {
         name: 'reframe-server-rendering',
@@ -22,23 +18,18 @@ function HapiServerRendering__create({getPages, reframeConfig={}}={}) {
     };
 
     function register(server, options) {
-        const {getRepage, doNotComputeEtag} = options;
-        assert_options(options);
-
-        const plugin_state = {};
-
         server.ext('onPreResponse', (request, h) =>
-            handle_request(request, h, getRepage, getPages, repage_plugins, plugin_state)
+            handle_request(request, h, getRepageObject)
         );
     }
 }
 
-async function handle_request(request, h, getRepage, getPages, repage_plugins, plugin_state) {
+async function handle_request(request, h, getRepageObject) {
     if( request_is_already_served(request) ) {
         return h.continue;
     }
 
-    const repage_object = get_repage_object(getRepage, getPages, repage_plugins, plugin_state);
+    const repage_object = getRepageObject();
 
     const html = await compute_html(request, repage_object);
 
@@ -46,54 +37,29 @@ async function handle_request(request, h, getRepage, getPages, repage_plugins, p
         return h.continue;
     }
 
-    const response = compute_response(html, doNotComputeEtag, h);
+    const response = compute_response(html, h);
     return response;
 }
 
-function get_repage_plugins(reframeConfig) {
-    processReframeConfig(reframeConfig);
-    const {repage_plugins} = reframeConfig._processed;
-    assert_internal(repage_plugins.constructor===Array);
-    return repage_plugins;
+async function compute_html(request, repage_object) {
+    const uri = request.url.href;
+    assert_internal(uri && uri.constructor===String, uri);
+
+    let {html, renderToHtmlIsMissing} = await getPageHtml(repage_object, uri, {canBeNull: true});
+    assert_html(html, renderToHtmlIsMissing);
+
+    return html;
 }
 
-function get_repage_object(getRepage, getPages, plugin_state) {
-    (() => {
-        if( getRepage ) {
-            const repage__new = getRepage();
-            if( repage__new === plugin_state.repage_object ) {
-                return;
-            }
-            assert_usage(repage__new, repage__new);
-            plugin_state.repage_object = repage__new;
-            return plugin_state.repage_object;
-        }
-
-        if( getPages ) {
-            const pages__new = getPages();
-            if( pages__new === plugin_state.pages ) {
-                return;
-            }
-            assert_usage(pages__new.constructor===Array, pages__new);
-            plugin_state.pages = pages__new;
-            plugin_state.repage_object = create_repage_object(pages__new);
-            return;
-        }
-        assert_internal(false);
-    })();
-
-    assert_internal(plugin_state.repage_object.isRepageObject);
-    return plugin_state.repage_object;
+function compute_response(html, h) {
+    const response = h.response(html);
+    response.type('text/html');
+    add_etag_header(response);
+    return response;
 }
 
-function create_repage_object(pages_) {
-    assert_internal(pages_);
-
-    const repage_object = new Repage();
-    repage_object.addPlugins(repage_plugins);
-    repage_object.addPages(pages_);
-
-    return repage_object;
+function add_etag_header(response) {
+    response.etag(compute_source_code_hash(response.source));
 }
 
 function request_is_already_served() {
@@ -109,37 +75,4 @@ function assert_html(html, renderToHtmlIsMissing) {
     assert_internal([true, false].includes(renderToHtmlIsMissing));
     assert_internal(!renderToHtmlIsMissing || html===null);
     assert_usage(renderToHtmlIsMissing===false);
-}
-
-function compute_response(html, doNotComputeEtag, h) {
-    const response = h.response(html);
-    response.type('text/html');
-    add_etag_header(response, doNotComputeEtag);
-    return response;
-}
-
-function add_etag_header(response, doNotComputeEtag) {
-    if( doNotComputeEtag ) {
-        return;
-    }
-    response.etag(compute_source_code_hash(response.source));
-}
-
-async function compute_html(request, repage_object) {
-    const uri = request.url.href;
-    assert_internal(uri && uri.constructor===String, uri);
-
-    let {html, renderToHtmlIsMissing} = await getPageHtml(repage_object, uri, {canBeNull: true});
-    assert_html(html, renderToHtmlIsMissing);
-
-    return html;
-}
-
-function assert_options(options) {
-    const {getRepage, doNotComputeEtag} = options;
-    assert_usage(
-        getPages || getRepage,
-        options,
-        "Options object (which is printed above) should provide `getPages` or `getRepage`."
-    );
 }
--- server/getReframeHapiPlugins.js
+++ server/getReframeHapiPlugins.js
@@ -1,4 +1,4 @@
-const {HapiServerRendering__create} = require('./HapiServerRendering');
+const {HapiPlugin_ServerRendering__create} = require('./HapiServerRendering');
 const buildDefault = require('@reframe/build');
 const {get_context} = require('@reframe/build/utils/get_context');
 const {generate_HapiServeBrowserAssets} = require('@rebuild/build/utils/HapiServeBrowserAssets');
@@ -7,48 +7,79 @@ const assert = require('reassert');
 const assert_internal = assert;
 const assert_usage = assert;
 
+const {processReframeConfig} = require('@reframe/utils');
+const Repage = require('@repage/core');
+
 module.exports = {getReframeHapiPlugins};
 
 async function getReframeHapiPlugins({
     context = get_context(),
     build = buildDefault,
-    reframeConfig,
+    reframeConfig={},
     ...build_opts
 }) {
-    /*
-    assert_usage(
-        build !== buildDefault || build_opts.pagesDir,
-        "Provide either argument `build` or `pagesDir`."
-    );
-    */
-    let pages;
-    let HapiServeBrowserAssets;
-    let browserDistPath;
+    const build_state = {};
     await build({
         onBuild: async args => {
             assert_internal(args.browserDistPath);
             assert_internal(args.pages);
             assert_usage(
                 !browserDistPath || browserDistPath===args.browserDistPath,
-                "We expect the served `dist/` directory to always be at the same path"
+                "The directory holding the static assets isn't expected to move.",
+                "Yet it is moving from `"+browserDistPath+"` to `"+args.browserDistPath+"`."
             );
-            if( ! browserDistPath ) {
-                browserDistPath = args.browserDistPath;
-                HapiServeBrowserAssets = generate_HapiServeBrowserAssets(args.browserDistPath);
-            }
-            pages = args.pages;
+            build_state.browserDistPath = args.browserDistPath;
+            build_state.pages = args.pages;
         },
         context,
         reframeConfig,
         ...build_opts,
     });
-    assert_internal(HapiServeBrowserAssets);
-    assert_internal(pages);
+    assert_internal(build_state.browserDistPath);
+    assert_internal(build_state.pages);
 
-    const HapiServerRendering = HapiServerRendering__create({getPages: () => pages, reframeConfig});
+    const HapiServeBrowserAssets = generate_HapiServeBrowserAssets(build_state.browserDistPath);
+    const HapiServerRendering = get_HapiPluginSSR(build_state, reframeConfig);
 
     return {
         HapiServerRendering,
         HapiServeBrowserAssets,
     };
+}
+
+function get_HapiPluginSSR(build_state, reframeConfig) {
+    const repage_plugins = get_repage_plugins(reframeConfig);
+
+    const cache = {};
+
+    const HapiServerRendering = HapiPlugin_ServerRendering__create(() => get_repage_object(build_state.pages, cache, repage_plugins));
+    return HapiServerRendering;
+}
+
+function get_repage_plugins(reframeConfig) {
+    processReframeConfig(reframeConfig);
+    const {repage_plugins} = reframeConfig._processed;
+    assert_internal(repage_plugins.constructor===Array);
+    return repage_plugins;
+}
+
+function create_repage_object(pages_) {
+    assert_internal(pages_);
+
+    const repage_object = new Repage();
+    repage_object.addPlugins(repage_plugins);
+    repage_object.addPages(pages_);
+
+    return repage_object;
+}
+
+function get_repage_object(pages, cache, repage_plugins) {
+    assert_internal(pages.constructor===Array, pages);
+    if( pages !== cache.pages ) {
+        cache.pages = pages;
+        cache.repage_object = create_repage_object(pages, repage_plugins);
+    }
+
+    assert_internal(cache.repage_object.isRepageObject);
+    return cache.repage_object;
 }
