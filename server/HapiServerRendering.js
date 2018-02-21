bea5ca64e869ac8c5cfb5c5d7ff5d7471315c569
Romuald Brillout <github.com@brillout.com>
2018-02-21 17:56:53 +0100

    make code more readable

--- server/HapiServerRendering.js
+++ server/HapiServerRendering.js
@@ -13,74 +13,87 @@ module.exports = {HapiServerRendering__create};
 
 
 function HapiServerRendering__create({getPages, reframeConfig={}}={}) {
-    processReframeConfig(reframeConfig);
+    const repage_plugins = get_repage_plugins(reframeConfig);
 
     return {
         name: 'reframe-server-rendering',
         multiple: false,
-        register: (server, options) => {
-            const {getRepage, doNotComputeEtag} = options;
-            assert_usage(getPages || getRepage, options);
-
-            let repage;
-            let pages;
-
-            server.ext('onPreResponse', onPreResponse);
-
-            return;
-
-            async function onPreResponse(request, h) {
-                if( request_is_already_served(request) ) {
-                    return h.continue;
-                }
-
-                init_repage_object();
-
-                const html = await compute_html(request, repage);
-
-                if( html === null ) {
-                    return h.continue;
-                }
-
-                return compute_response(html, doNotComputeEtag, h);
-            }
-
-            function init_repage_object() {
-                if( getRepage ) {
-                    const repage__new = getRepage();
-                    if( repage__new === repage ) {
-                        return;
-                    }
-                    assert_usage(repage__new, repage__new);
-                    repage = repage__new;
-                    return;
-                }
-
-                if( getPages ) {
-                    const pages__new = getPages();
-                    if( pages__new === pages ) {
-                        return;
-                    }
-                    assert_usage(pages__new.constructor===Array, pages__new);
-                    pages = pages__new;
-                    repage = create_repage_object(pages);
-                    return;
-                }
-
-                assert_internal(false);
-            }
-
-            function create_repage_object(pages_) {
-                assert_internal(pages_);
-
-                const repage = new Repage();
-                repage.addPlugins(reframeConfig._processed.repage_plugins);
-                repage.addPages(pages_);
-
-                return repage;
-            }
-        },
+        register,
     };
+
+    function register(server, options) {
+        const {getRepage, doNotComputeEtag} = options;
+        assert_options(options);
+
+        const plugin_state = {};
+
+        server.ext('onPreResponse', (request, h) =>
+            handle_request(request, h, getRepage, getPages, repage_plugins, plugin_state)
+        );
+    }
+}
+
+async function handle_request(request, h, getRepage, getPages, repage_plugins, plugin_state) {
+    if( request_is_already_served(request) ) {
+        return h.continue;
+    }
+
+    const repage_object = get_repage_object(getRepage, getPages, repage_plugins, plugin_state);
+
+    const html = await compute_html(request, repage_object);
+
+    if( html === null ) {
+        return h.continue;
+    }
+
+    const response = compute_response(html, doNotComputeEtag, h);
+    return response;
+}
+
+function get_repage_plugins(reframeConfig) {
+    processReframeConfig(reframeConfig);
+    const {repage_plugins} = reframeConfig._processed;
+    assert_internal(repage_plugins.constructor===Array);
+    return repage_plugins;
+}
+
+function get_repage_object(getRepage, getPages, plugin_state) {
+    (() => {
+        if( getRepage ) {
+            const repage__new = getRepage();
+            if( repage__new === plugin_state.repage_object ) {
+                return;
+            }
+            assert_usage(repage__new, repage__new);
+            plugin_state.repage_object = repage__new;
+            return plugin_state.repage_object;
+        }
+
+        if( getPages ) {
+            const pages__new = getPages();
+            if( pages__new === plugin_state.pages ) {
+                return;
+            }
+            assert_usage(pages__new.constructor===Array, pages__new);
+            plugin_state.pages = pages__new;
+            plugin_state.repage_object = create_repage_object(pages__new);
+            return;
+        }
+        assert_internal(false);
+    })();
+
+    assert_internal(plugin_state.repage_object.isRepageObject);
+    return plugin_state.repage_object;
+}
+
+function create_repage_object(pages_) {
+    assert_internal(pages_);
+
+    const repage_object = new Repage();
+    repage_object.addPlugins(repage_plugins);
+    repage_object.addPages(pages_);
+
+    return repage_object;
 }
 
 function request_is_already_served() {
@@ -112,12 +125,21 @@ function add_etag_header(response, doNotComputeEtag) {
     response.etag(compute_source_code_hash(response.source));
 }
 
-async function compute_html(request, repage) {
+async function compute_html(request, repage_object) {
     const uri = request.url.href;
     assert_internal(uri && uri.constructor===String, uri);
 
-    let {html, renderToHtmlIsMissing} = await getPageHtml(repage, uri, {canBeNull: true});
+    let {html, renderToHtmlIsMissing} = await getPageHtml(repage_object, uri, {canBeNull: true});
     assert_html(html, renderToHtmlIsMissing);
 
     return html;
+}
+
+function assert_options(options) {
+    const {getRepage, doNotComputeEtag} = options;
+    assert_usage(
+        getPages || getRepage,
+        options,
+        "Options object (which is printed above) should provide `getPages` or `getRepage`."
+    );
 }
