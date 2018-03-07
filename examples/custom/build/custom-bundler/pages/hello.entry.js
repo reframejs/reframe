fb4dfa11ff43866bc41858a1d0903fcf2a407bc8
Romuald Brillout <github.com@brillout.com>
2018-03-07 19:47:13 +0100

    adapt examples to new build system

--- examples/custom/build/custom-bundler/pages/hello.entry.js
+++ examples/custom/build/custom-bundler/pages/hello.entry.js
@@ -1,4 +1,24 @@
-import hydratePage from '@reframe/browser/hydratePage';
 import HelloPage from './hello.mjs';
 
-hydratePage(HelloPage);
+import Repage from '@repage/core';
+
+import RepageRenderer from '@repage/renderer';
+import RepageRendererReact from '@repage/renderer-react';
+import RepageRouterPathToRegexp from '@repage/router-path-to-regexp';
+import RepageNavigator from '@repage/navigator';
+
+import hydratePage from '@repage/browser/hydratePage';
+
+
+const repage = new Repage();
+
+repage.addPlugins([
+    RepageRenderer,
+    RepageRendererReact,
+    RepageRouterPathToRegexp,
+    RepageNavigator,
+]);
+
+(async () => {
+    await hydratePage(repage, HelloPage);
+})();
