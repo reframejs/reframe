0157c70856ae85a45fbb02277c307482add156cb
Romuald Brillout <github.com@brillout.com>
2018-03-28 16:41:47 +0200

    externalize cli code to @reframe/run

--- helpers/cli/templates/coreFilesTemplate.js
+++ helpers/cli/templates/coreFilesTemplate.js
@@ -1,23 +1,31 @@
 module.exports = {jsonPkgTemplate, reframeConfigTemplate};
 
 function jsonPkgTemplate(projectName) {
-    let template =
+    const assert_internal = require('reassert/internal');
+    const pkg = require('../package.json');
+    const {version} = pkg;
+    assert_internal(version);
+
+    const template = (
 `{
     "name": "${projectName}",
     "dependencies": {
+        "@reframe/run": "^${version}",
         "react": "^16.2.0"
     },
     "private": true
 }
-`;
+`
+    );
 
     return template;
 }
 
 function reframeConfigTemplate() {
-    let template =
+    const template = (
 `module.exports = {};
 `
+    );
 
     return template;
 }
