5684512ef35cc5a472b28d68c0cb6c381f6bb9ed
Romuald Brillout <github.com@brillout.com>
2018-04-03 16:14:11 +0200

    implement eject

--- examples/basics/pages/TrackingPage.entry.js
+++ examples/basics/pages/TrackingPage.entry.js
@@ -1,4 +1,4 @@
-import hydratePage from '@reframe/browser/hydratePage';
+//import hydratePage from '@reframe/browser/hydratePage';
 import TrackingPage from './TrackingPage.js';
 
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
@@ -9,7 +9,7 @@ ga('send', 'pageview');
     const before = new Date();
     // We are reusing the `TrackingPage` page confg but
     // we could also use another page definition.
-    await hydratePage(TrackingPage);
+    //await hydratePage(TrackingPage);
     const after = new Date();
     ga('send', 'event', {eventAction: 'page hydration time', eventValue: after - before});
 })();
