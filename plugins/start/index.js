600bf6842fb9456036f7498f42b2eb5d118e5d41
Romuald Brillout <github.com@brillout.com>
2018-04-11 16:45:06 +0200

    [code-removal] remove package @reframe/run

--- plugins/start/index.js
+++ plugins/start/index.js
@@ -1,7 +1,3 @@
-const getProjectConfig = require('@reframe/utils/getProjectConfig');
-const assert_internal = require('reassert/internal');
-const assert_usage = require('reassert/usage');
-
 module.exports = startCommands;
 
 function startCommands() {
@@ -21,51 +17,74 @@ function startCommands() {
                         description: "prints build and page information",
                     },
                 ],
-                action: async ({production, log}) => {
-                    if( production ) {
-                        process.env['NODE_ENV'] = 'production';
-                    }
-
-                    setOptions({log})
-                    /*
-                    const build = require(resolvePackagePath('@reframe/build'));
-                    await build({log});
-                    */
-                    await require(resolvePackagePath('@reframe/build'));
-
-                    const server = require(resolvePackagePath('@reframe/server'));
-                    server({log});
-                },
+                action,
             }
         ],
     };
 }
 
-function setOptions({log}) {
-    const projectConfig = getProjectConfig();
-    projectConfig.log = {
-        verbose: !!log,
-    };
-}
+async function action() {
+    const getProjectConfig = require('@reframe/utils/getProjectConfig');
+    const relative_to_homedir = require('@brillout/relative-to-homedir');
+    const assert_internal = require('reassert/internal');
+    const assert_usage = require('reassert/usage');
+    const chalk = require('chalk');
 
-function resolvePackagePath(packageName) {
-    const projectConfig = getProjectConfig();
-    const {projectRootDir} = projectConfig.projectFiles;
-    assert_internal(projectRootDir);
+    await runStart.apply(null, arguments);
 
-    let packagePath;
-    try {
-        packagePath = require.resolve(packageName, {paths: [projectRootDir]});
-    } catch(err) {
-        if( err.code!=='MODULE_NOT_FOUND' ) throw err;
-        assert_usage(
-            false,
-            "Package `"+packageName+"` is missing.",
-            "You need to install it: `npm install "+packageName+"`.",
-            "Project in question: `"+projectRootDir+"`.",
-        );
+    return;
+
+    async function runStart({production, log}) {
+        if( production ) {
+            process.env['NODE_ENV'] = 'production';
+        }
+
+        const projectConfig = getProjectConfig();
+
+        log_found_file(projectConfig.projectFiles.reframeConfigFile, 'Reframe config');
+        log_found_file(projectConfig.projectFiles.pagesDir, 'Pages directory');
+
+        projectConfig.log = {
+            verbose: !!log,
+        };
+
+        /*
+        const build = require(resolvePackagePath('@reframe/build'));
+        await build({log});
+        */
+        await require(resolvePackagePath('@reframe/build', projectConfig.projectFiles.projectRootDir));
+
+        const server = require(resolvePackagePath('@reframe/server', projectConfig.projectFiles.projectRootDir));
+        server({log});
     }
 
-    assert_internal(packagePath);
-    return packagePath;
+    function resolvePackagePath(packageName, projectRootDir) {
+        assert_internal(projectRootDir);
+
+        let packagePath;
+        try {
+            packagePath = require.resolve(packageName, {paths: [projectRootDir]});
+        } catch(err) {
+            if( err.code!=='MODULE_NOT_FOUND' ) throw err;
+            assert_usage(
+                false,
+                "Package `"+packageName+"` is missing.",
+                "You need to install it: `npm install "+packageName+"`.",
+                "Project in question: `"+projectRootDir+"`.",
+            );
+        }
+
+        assert_internal(packagePath);
+        return packagePath;
+    }
+
+    function log_found_file(file_path, description) {
+        if( file_path ) {
+            console.log(green_checkmark()+' '+description+' found at '+relative_to_homedir(file_path));
+        }
+    }
+
+    function green_checkmark() {
+        return chalk.green('\u2714');
+    }
 }
