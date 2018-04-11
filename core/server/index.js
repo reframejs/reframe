9a91f3a9e0801c0df52bb3efd93eecc3bbe27fd4
Romuald Brillout <github.com@brillout.com>
2018-04-11 16:49:27 +0200

    [code-removal] remove old server code

--- core/server/index.js
+++ core/server/index.js
@@ -1,17 +1,11 @@
 const Hapi = require('hapi');
 const HapiPluginServerRendering = require('./HapiPluginServerRendering');
 const HapiPluginStaticAssets = require('./HapiPluginStaticAssets');
-const getProjectConfig = require('@reframe/utils/getProjectConfig');
 const chalk = require('chalk');
 
 module.exports = startServer;
 
 async function startServer() {
-    /*
-    const projectConfig = getProjectConfig();
-
-    const server = Hapi.Server(projectConfig.hapiServerConfig);
-    */
     const server = Hapi.Server({
         port: 3000,
         debug: {
