9a943f42bee696e20ec1cb91a6a28502fc86f30c
Romuald Brillout <github.com@brillout.com>
2018-04-18 18:47:28 +0200

    implement eject

--- plugins/eject/index.js
+++ plugins/eject/index.js
@@ -17,9 +17,56 @@ function ejectCommand() {
 async function runEject(ejectableName) {
     const ejectables = getEjectables();
 
-    if( ! ejectables[ejectableName] ) {
+    const ejectableSpec = ejectables[ejectableName];
+    if( ! ejectableSpec ) {
         console.log('No ejectable `'+ejectableName+'` found.');
         printEjectables(ejectables);
+    }
+
+    const detective = require('detective');
+    const builtins = require('builtins')();
+    const assert_usage = require('reassert/usage');
+    const getProjectConfig = require('@reframe/utils/getProjectConfig');
+    const pathModule = require('path');
+    const fs = require('fs');
+    const assert_plugin = assert_usage;
+
+    const {configFileMove} = ejectableSpec;
+    if( configFileMove ) {
+        const projectConfig = getProjectConfig();
+        Object.entries(configFileMove)
+        .forEach(([configProp, newFilePath]) => {
+            const oldPath = projectConfig[configProp];
+            assert_plugin(oldPath);
+            const fileContent = fs__read(oldPath);
+            const requires = detective(fileContent);
+
+            const deps = {};
+            requires
+            .map(requireString => {
+                if( requireString.startsWith('.') ) {
+                    pathModule.resolve(requireString);
+                }
+                fileContent.replace(requireString
+                console.log(fileContent, found);
+            })
+            .forEach(requireString => {
+                
+            });
+        });
+        return;
+    }
+
+    assert_plugin(
+        false,
+        ejectableSpec,
+        "Wrong ejectable spec"
+    );
+
+    return;
+
+    function fs__read(filePath) {
+        return fs.readFileSync(filePath, 'utf8');
     }
 }
 
