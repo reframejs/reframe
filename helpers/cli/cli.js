952fd16249a41a0f4cf880534c83c17c5977936f
Romuald Brillout <github.com@brillout.com>
2018-05-07 14:04:41 +0200

    improve details

--- helpers/cli/cli.js
+++ helpers/cli/cli.js
@@ -6,14 +6,13 @@ const ora = require('ora');
 const loading_spinner = ora();
 loading_spinner.start();
 
-const program = require('commander');
 const cliUtils = require('@reframe/utils/cliUtils');
 const assert_usage = require('reassert/usage');
+const assert_internal = require('reassert/internal');
 const {colorEmphasisLight, strTable, strDir, strFile, colorFile, colorPkg, colorDir, colorError} = require('@brillout/cli-theme');
 const getUserDir = require('@brillout/get-user-dir');
 const getProjectConfig = require('@reframe/utils/getProjectConfig');
 
-const INDENT = '  ';
 const FALLBACK_PLUGIN = '@reframe/init';
 
 const cwd = process.cwd();
@@ -24,160 +23,201 @@ const projectConfig = getProjectConfig({projectNotRequired: true, pluginRequired
 const isProject = projectConfig.projectFiles.projectRootDir;
 
 if( ! isProject ) {
-    const initCommands = require(FALLBACK_PLUGIN);
     projectConfig.addPlugin(
-        initCommands()
-    );
-} else {
-    const {_rootPluginNames, cli_commands, projectFiles: {reframeConfigFile}} = projectConfig;
-    assert_usage(
-        cli_commands.length>0,
-        colorError("No commands found."),
-        "Project found at "+colorDir(strDir(projectConfig.projectFiles.projectRootDir))+".",
-        reframeConfigFile ? (
-            "Reframe config found at "+colorFile(strFile(reframeConfigFile))+"."
-        ) : (
-            "No Reframe config file found."
-        ),
-        "Loaded plugins: "+_rootPluginNames.map(pluginName => colorPkg(pluginName)).join(', ')+'.',
-        "None of the loaded plugins are adding commands."
+        require(FALLBACK_PLUGIN)()
     );
 }
 
-let noCommand = true;
+assert_at_least_one_command();
 
-program
-.command('help [command]')
-.description('Output usage information')
-.action(commandName => {
-    noCommand = false;
-    printHelp(commandName);
-});
-
-const commandList = {};
-
-projectConfig
-.cli_commands
-.forEach(cmdSpec => {
-    let commandLine = cmdSpec.name;
-    if( cmdSpec.param ) {
-        commandLine += ' '+cmdSpec.param;
-    }
-    let cmd = (
-        program
-        .command(commandLine)
-    );
-
-    commandList[cmdSpec.name] = {...cmdSpec, commandLine};
-
-    (cmdSpec.options||[])
-    .forEach(opt => {
-        cmd.option(opt.name, opt.description);
-    });
-
-    cmd
-    .description(cmdSpec.description)
-    .action(function(options) {
-        noCommand = false;
-        if( process.argv.includes('-h') || process.argv.includes('--help') ) {
-            printHelp(cmdSpec.name)
-            return;
-        }
-        cmdSpec.action(options, {printHelp: () => printHelp(cmdSpec.name)});
-    });
-});
-
-program
-.arguments('<command>')
-.action(commandName => {
-    noCommand = false;
-    printInvalidCommand(commandName);
-});
-
-// Disable commander's default help behavior
-program.option('-h, --help');
+const {runProgram} = initProgram();
 
 loading_spinner.stop();
 
-program.parse(process.argv);
+runProgram();
 
-if( noCommand ) {
-    printHelp();
-}
 
-function printHelp(commandName) {
-    if( commandName ) {
-        const cmdSpec = commandList[commandName];
-        if( ! cmdSpec ) {
-            printInvalidCommand(commandName);
-            return;
-        }
-        printHelpCommand(cmdSpec);
-        return;
-    }
+function initProgram() {
+    const program = require('commander');
 
-    printHelpProgram();
-}
+    const INDENT = '  ';
 
-function printHelpCommand(cmdSpec) {
-    let usageLog = INDENT+'Usage: reframe '+cmdSpec.commandLine;
+    let noCommand = true;
 
-    const optionsSpec = cmdSpec.options || [];
+    addHelpCommand();
 
-    let optionsLog = '';
-    if( optionsSpec.length>0 ) {
-        optionsSpec
-        .forEach(opt => {
-            usageLog += ' ['+opt.name.split(', ').join('|')+']';
+    const commandList = addCommands(projectConfig.cli_commands);
+
+    addFallbackCommand();
+
+    disableHelp();
+
+    return {
+        runProgram: () => {
+            program.parse(process.argv);
+
+            if( noCommand ) {
+                printHelp();
+            }
+        },
+    };
+
+    function addCommands(commandArray) {
+        const commandList = {};
+
+        projectConfig
+        .cli_commands
+        .forEach(cmdSpec => {
+            let commandLine = cmdSpec.name;
+            if( cmdSpec.param ) {
+                commandLine += ' '+cmdSpec.param;
+            }
+            const cmd = (
+                program
+                .command(commandLine)
+            );
+
+            commandList[cmdSpec.name] = {...cmdSpec, commandLine};
+
+            (cmdSpec.options||[])
+            .forEach(opt => {
+                cmd.option(opt.name, opt.description);
+            });
+
+            cmd
+            .description(cmdSpec.description)
+            .action(function(options) {
+                noCommand = false;
+                if( process.argv.includes('-h') || process.argv.includes('--help') ) {
+                    printHelp(cmdSpec.name)
+                    return;
+                }
+                cmdSpec.action(options, {printHelp: () => printHelp(cmdSpec.name)});
+            });
         });
 
-        optionsLog += '\n';
-        optionsLog += INDENT+'Options:\n';
-        optionsLog += strTable(
+        return commandList;
+    }
+
+    function addHelpCommand() {
+        program
+        .command('help [command]')
+        .description('Output usage information')
+        .action(commandName => {
+            noCommand = false;
+            printHelp(commandName);
+        });
+    }
+
+    function addFallbackCommand() {
+        program
+        .arguments('<command>')
+        .action(commandName => {
+            noCommand = false;
+            printInvalidCommand(commandName);
+        });
+    }
+
+    function disableHelp() {
+        // Disable commander's default help behavior
+        program.option('-h, --help');
+    }
+
+    function printHelp(commandName) {
+        if( commandName ) {
+            const cmdSpec = commandList[commandName];
+            if( ! cmdSpec ) {
+                printInvalidCommand(commandName);
+                return;
+            }
+            printHelpCommand(cmdSpec);
+            return;
+        }
+
+        printHelpProgram();
+    }
+
+    function printHelpCommand(cmdSpec) {
+        let usageLog = INDENT+'Usage: reframe '+cmdSpec.commandLine;
+
+        const optionsSpec = cmdSpec.options || [];
+
+        let optionsLog = '';
+        if( optionsSpec.length>0 ) {
             optionsSpec
-            .map(opt => [opt.name, opt.description]),
+            .forEach(opt => {
+                usageLog += ' ['+opt.name.split(', ').join('|')+']';
+            });
+
+            optionsLog += '\n';
+            optionsLog += INDENT+'Options:\n';
+            optionsLog += strTable(
+                optionsSpec
+                .map(opt => [opt.name, opt.description]),
+                {indent: INDENT+INDENT}
+            );
+            optionsLog += '\n';
+        }
+
+        console.log();
+        console.log(INDENT+colorEmphasisLight('reframe '+cmdSpec.name)+' - '+cmdSpec.description);
+        console.log();
+        console.log(usageLog);
+        console.log(optionsLog);
+        if( cmdSpec.getHelp ) {
+            cmdSpec.getHelp();
+        }
+    }
+
+    function printHelpProgram() {
+        console.log();
+        console.log(INDENT+'Usage: reframe [command]');
+        console.log();
+        console.log(INDENT+'Commands:');
+        console.log(strTable(
+            Object.entries(commandList)
+            .map(([commandName, cmdSpec]) => [colorEmphasisLight(commandName), cmdSpec.description]),
             {indent: INDENT+INDENT}
-        );
-        optionsLog += '\n';
+        ));
+        console.log();
+        if( ! isProject ) {
+            console.log(INDENT+'Commands provided by plugin '+FALLBACK_PLUGIN+'. (No project found at '+strDir(cwd)+'.)');
+        } else {
+         // const emphasize = colorEmphasisLight;
+            const emphasize = s => s;
+            console.log(INDENT+'Commands provided by '+cliUtils.getRootPluginsLog(projectConfig, emphasize)+' of '+cliUtils.getProjectRootLog(projectConfig, emphasize)+'.');
+        }
+        console.log();
+        console.log(INDENT+'Run '+colorEmphasisLight('reframe help <command>')+' for more information on specific commands.');
+        console.log();
     }
 
-    console.log();
-    console.log(INDENT+colorEmphasisLight('reframe '+cmdSpec.name)+' - '+cmdSpec.description);
-    console.log();
-    console.log(usageLog);
-    console.log(optionsLog);
-    if( cmdSpec.getHelp ) {
-        cmdSpec.getHelp();
+    function printInvalidCommand(commandName) {
+        console.log();
+        console.log("Command "+colorEmphasisLight(commandName)+" doesn't exist.");
+        console.log();
+        console.log('Run '+colorEmphasisLight('reframe help')+' for the list of commands.');
+        console.log();
     }
 }
 
-function printHelpProgram() {
-    console.log();
-    console.log(INDENT+'Usage: reframe [command]');
-    console.log();
-    console.log(INDENT+'Commands:');
-    console.log(strTable(
-        Object.entries(commandList)
-        .map(([commandName, cmdSpec]) => [colorEmphasisLight(commandName), cmdSpec.description]),
-        {indent: INDENT+INDENT}
-    ));
-    console.log();
+function assert_at_least_one_command() {
+    const {_rootPluginNames, cli_commands, projectFiles: {reframeConfigFile}} = projectConfig;
+
     if( ! isProject ) {
-        console.log(INDENT+'Commands provided by plugin '+FALLBACK_PLUGIN+'. (No project found at '+strDir(cwd)+'.)');
+        assert_internal(cli_commands.length>0);
     } else {
-     // const emphasize = colorEmphasisLight;
-        const emphasize = s => s;
-        console.log(INDENT+'Commands provided by '+cliUtils.getRootPluginsLog(projectConfig, emphasize)+' of '+cliUtils.getProjectRootLog(projectConfig, emphasize)+'.');
+        assert_usage(
+            cli_commands.length>0,
+            colorError("No commands found."),
+            "Project found at "+colorDir(strDir(projectConfig.projectFiles.projectRootDir))+".",
+            reframeConfigFile ? (
+                "Reframe config found at "+colorFile(strFile(reframeConfigFile))+"."
+            ) : (
+                "No Reframe config file found."
+            ),
+            "Loaded plugins: "+_rootPluginNames.map(pluginName => colorPkg(pluginName)).join(', ')+'.',
+            "None of the loaded plugins are adding commands."
+        );
     }
-    console.log();
-    console.log(INDENT+'Run '+colorEmphasisLight('reframe help <command>')+' for more information on specific commands.');
-    console.log();
-}
-
-function printInvalidCommand(commandName) {
-    console.log();
-    console.log("Command "+colorEmphasisLight(commandName)+" doesn't exist.");
-    console.log();
-    console.log('Run '+colorEmphasisLight('reframe help')+' for the list of commands.');
-    console.log();
 }
