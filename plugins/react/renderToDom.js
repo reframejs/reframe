c4d77c952853a1ba5b01acdc58db4089c2c01116
Romuald Brillout <github.com@brillout.com>
2018-05-08 17:08:09 +0200

    [code-removal] improve repage - always assume that page is already rendered to html and can be hydrated

--- plugins/react/renderToDom.js
+++ plugins/react/renderToDom.js
@@ -6,39 +6,7 @@ module.exports = renderToDom;
 async function renderToDom({pageConfig, initialProps}) {
     const reactElement = getReactElement({pageConfig, initialProps});
 
-    const container = get_or_create_container(containerId);
+    const container = document.getElementById(containerId);
 
-    await render(reactElement, container);
-}
-
-// TODO: always assume that page is already rendered to HTML
-
-async function render(reactElement, container) {
-    const isAlreadyRendered = container.firstChild !== null;
-
-    if( isAlreadyRendered ) {
-        ReactDOM.hydrate(reactElement, container);
-    } else {
-        ReactDOM.render(reactElement, container);
-    }
-}
-
-function get_or_create_container(containerId) {
-    let container = document.getElementById(containerId);
-    if( ! container ) {
-        container = create_dom_element({domId: containerId});
-    }
-    return container;
-}
-
-function create_dom_element({domId}) {
-    const el = document.createElement('div');
-    el.id = domId;
-    const firstChild = document.body.firstChild;
-    if( ! firstChild ) {
-        document.body.appendChild(el);
-    } else {
-        document.body.insertBefore(el, firstChild);
-    }
-    return el;
+    ReactDOM.hydrate(reactElement, container);
 }
