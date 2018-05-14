bc1cfe49b7518ca3c7f5207bdccb50c6ae04e2e5
Romuald Brillout <github.com@brillout.com>
2018-05-14 20:21:20 +0200

    improve details

--- plugins/init/scaffold/views/Header/Header.js
+++ plugins/init/scaffold/views/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Header.css';
+import logoUrl from './logo.svg';
 
 const Header = () => (
     <div className="header">
+        <img src={logoUrl} className="logo"/>
         <a href="/">Sample Project</a>
     </div>
 );
