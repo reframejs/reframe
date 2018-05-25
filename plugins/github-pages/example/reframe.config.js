d8f7163e34a0c1db3828bce2ab584513a10e6f17
Romuald Brillout <github.com@brillout.com>
2018-05-25 23:35:55 +0200

    improve details

--- plugins/github-pages/example/reframe.config.js
+++ plugins/github-pages/example/reframe.config.js
@@ -1,9 +1,4 @@
-const githubPages = require('..');
-
 module.exports = {
-    plugins: [
-        githubPages()
-    ],
     githubPagesRepository: {
         remote: 'git@github.com:brillout/reframe-github-pages-test',
         branch: 'master', // optional, default is `master`
