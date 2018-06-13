aa21a3fb0f0122f28034130161419581d43ea1a8
Romuald Brillout <github.com@brillout.com>
2018-06-13 14:05:22 +0200

    improve eject logic

--- plugins/hapi/index.js
+++ plugins/hapi/index.js
@@ -18,10 +18,23 @@ module.exports = {
 
 
 function getEjectables() {
+    const ejectName_ServerIntegration = 'server-integration';
     return [
         {
             name: 'server',
-            description: 'Eject the code that creates the Node.js/hapi server.',
+            description: '[basic] Eject the code that creates the Node.js/hapi server.',
+            descriptionLong: [
+                'Ejects the code that creates a hapi server instance.',
+                '',
+                'This is the right eject if you want to for example:',
+                ' - Add API endpoints',
+                ' - Add routes to the server',
+                ' - Change server configuration (e.g. the server port)',
+                ' - Add hapi plugins',
+                '',
+                'If you want to use another web framework then also eject the `'+ejectName_ServerIntegration+'` ejectable, see `$ reframe help eject '+ejectName_ServerIntegration+'`.',
+            ].join('\n'),
+            listingPrio: 100,
             actions: [
                 {
                     targetDir: 'server/',
@@ -31,7 +44,7 @@ function getEjectables() {
             ],
         },
         {
-            name: 'server-integration',
+            name: ejectName_ServerIntegration,
             description: 'Eject the hapi plugin that integrates hapi with Reframe.',
             actions: [
                 {
