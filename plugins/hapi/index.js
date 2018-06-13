92119871f8e4ebeb77603006c9e63c04f5e1257e
Romuald Brillout <github.com@brillout.com>
2018-06-13 14:19:11 +0200

    improve eject logic

--- plugins/hapi/index.js
+++ plugins/hapi/index.js
@@ -18,13 +18,16 @@ module.exports = {
 
 
 function getEjectables() {
-    const ejectName_ServerIntegration = 'server-integration';
+    const ejectName_server = 'server';
+    const ejectName_serverIntegration = 'server-integration';
     return [
         {
-            name: 'server',
+            name: ejectName_server,
             description: '[basic] Eject the code that creates the Node.js/hapi server.',
             descriptionLong: [
                 'Ejects the code that creates a hapi server instance.',
+                '',
+                'This is a very common ejectable and we strongly encourage you to eject it.',
                 '',
                 'This is the right eject if you want to for example:',
                 ' - Add API endpoints',
@@ -32,7 +35,7 @@ function getEjectables() {
                 ' - Change server configuration (e.g. the server port)',
                 ' - Add hapi plugins',
                 '',
-                'If you want to use another web framework then also eject the `'+ejectName_ServerIntegration+'` ejectable, see `$ reframe help eject '+ejectName_ServerIntegration+'`.',
+                'If you want to use another web framework instead of hapi, then also eject the `'+ejectName_serverIntegration+'` ejectable, see `$ reframe help eject '+ejectName_serverIntegration+'`.',
             ].join('\n'),
             listingPrio: 100,
             actions: [
@@ -44,8 +47,17 @@ function getEjectables() {
             ],
         },
         {
-            name: ejectName_ServerIntegration,
+            name: ejectName_serverIntegration,
             description: 'Eject the hapi plugin that integrates hapi with Reframe.',
+            descriptionLong: [
+                'Ejects the hapi plugin that implements the hapi <-> Reframe integration.',
+                '',
+                "This is an uncommon ejectable and chances are that you will never have to eject it.",
+                'However, it can be useful if you want to fully customize the server.',
+                'E.g. if you want to use another web framework instead of hapi.',
+                '',
+                'If you just want to make changes to the hapi server, then run `$ reframe eject '+ejectName_server+'` instead.'
+            ].join('\n'),
             actions: [
                 {
                     targetDir: 'server/',
