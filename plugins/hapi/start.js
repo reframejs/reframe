553980546a2e35cba6309490a883d7cdf8b169b1
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-20 16:38:13 +0300

    PORT env var.

--- plugins/hapi/start.js
+++ plugins/hapi/start.js
@@ -6,7 +6,7 @@ module.exports = start();
 
 async function start() {
     const server = Hapi.Server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         debug: {request: ['internal']},
     });
 
