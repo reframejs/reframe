2b95744e00879d26ffb8fd9789f1dfde7278fbf2
Romuald Brillout <github.com@brillout.com>
2018-06-21 16:01:22 +0200

    misc

--- helpers/dev-utils/npm-utils/checkIfPublished.js
+++ helpers/dev-utils/npm-utils/checkIfPublished.js
@@ -1,24 +1,46 @@
-const getPackages = require('./getPackages');
-const assert = require('reassert');
-const {symbolSuccess, colorError} = require('@brillout/cli-theme');
+const getNpmTags = require('./getNpmTags');
+const {symbolSuccess, colorError, indent, colorEmphasis} = require('@brillout/cli-theme');
 
 checkIfPublished();
 
 async function checkIfPublished() {
-    getPackages()
-    .forEach(async ({exec, packageName, packageVersion, packageNameAndVersion}) => {
-        const versions = await getVersions(packageName, exec);
-        assert(
-            versions.includes(packageVersion),
-            colorError("Not published: "+packageNameAndVersion)
-        );
-        console.log(symbolSuccess+packageNameAndVersion+' published.');
+    const {cumulated, packages} = await getNpmTags();
+
+    const referencePackage = packages[0];
+    const referenceTags = referencePackage.tags;
+
+    let hasError = false;
+
+    packages
+    .forEach(({packageName, tags}) => {
+        if( !hasEqualTags(tags, referenceTags) ){
+            hasError = true;
+            console.log(colorError("The two packages "+packageName+" and "+referencePackage.packageName+" have different tags:")),
+            console.log(tags);
+            console.log(referenceTags);
+            console.log();
+        }
     });
+
+    if( ! hasError ) {
+        console.log(symbolSuccess+'All packages have the same tags:');
+        console.log(
+            referenceTags
+            .map(({tagName, packageVersion}) =>
+                indent+indent+colorEmphasis(tagName)+' '+packageVersion
+            ).join('\n')
+        );
+    }
 }
 
-async function getVersions(packageName, exec) {
-    const {stdout} = await exec('npm', ['show', packageName, 'versions']);
-    const versions = eval(stdout);
-    assert(versions.constructor===Array, stdout);
-    return versions;
+function hasEqualTags(tags1, tags2) {
+    if( tags1.length !== tags2.length ) {
+        return false;
+    }
+    for(tag1 of tags1) {
+        if( ! tags2.find(tag2 => tag1.tagName === tag2.tagName && tag1.packageVersion === tag2.packageVersion) ) {
+            return false;
+        }
+    }
+    return true;
 }
