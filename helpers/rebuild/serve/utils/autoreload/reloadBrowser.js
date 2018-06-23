2f9a4798fb5fe67338a4330b69970c0e7c2f3849
Romuald Brillout <github.com@brillout.com>
2018-06-24 00:10:23 +0200

    misc

--- helpers/rebuild/serve/utils/autoreload/reloadBrowser.js
+++ helpers/rebuild/serve/utils/autoreload/reloadBrowser.js
@@ -1,4 +1,6 @@
 const WebSocket = require('ws');
+const assert_warning = require('reassert/warning');
+const {colorWarning} = require('@brillout/cli-theme');
 
 module.exports = reloadBrowser;
 
@@ -9,6 +11,7 @@ function reloadBrowser() {
     if( ! reloadSocket ) {
         reloadSocket = new WebSocket.Server({port: PORT});
     }
+    improveErrorHandling(reloadSocket);
     broadcast(reloadSocket, 'RELOAD');
 }
 
@@ -18,4 +21,17 @@ function broadcast(socket, data) {
           client.send(data);
       }
   });
+}
+
+function improveErrorHandling(reloadSocket) {
+    reloadSocket.on("error", err => {
+        if( ! ((err||{}).message||'').includes('EADDRINUSE') ) {
+            throw err;
+        }
+        assert_warning(
+            false,
+            colorWarning("Autoreload will not work."),
+            "Because the port "+PORT+" is already in use."
+        );
+    });
 }
