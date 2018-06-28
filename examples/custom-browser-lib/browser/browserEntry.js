822f1ad8d562103fae979b5d8385352ba50912fe
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-29 00:58:47 +0300

    Fixed 'jQuery is not defined'.

--- examples/custom-browser-lib/browser/browserEntry.js
+++ examples/custom-browser-lib/browser/browserEntry.js
@@ -1,6 +1,6 @@
 import browserConfig from '@brillout/browser-config';
 
-import './thirdparty/jquery-3.1.1/jquery-3.1.1.min';
+import './jquery-global'; // see https://stackoverflow.com/a/39820703/1855917
 import './thirdparty/semantic-ui-2.1.8/semantic.min';
 
 initBrowser();
