20712338f6fa4bebb0faffa75a0d266e319962fd
Romuald Brillout <github.com@brillout.com>
2018-07-04 22:50:39 +0200

    improve details

--- examples/custom-browser-lib/browser/browserInit.js
+++ examples/custom-browser-lib/browser/browserInit.js
@@ -6,5 +6,16 @@ import './thirdparty/semantic-ui-2.1.8/semantic.min.js';
 initBrowser();
 
 async function initBrowser() {
-    await browserConfig.hydratePage();
+    // Include pre-init code here
+
+    // Plugins can add init functions
+    // For example:
+    //  - page hydration (i.e. rendering of the page to the DOM)
+    //  - user tracking initialization
+    for(const initFunction of Object.values(browserConfig.initFunctions)) {
+        await initFunction();
+    }
+
+    // Include post-init code here
 }
+
