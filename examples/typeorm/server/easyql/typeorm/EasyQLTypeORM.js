032561acf42628788837185ee4976977038a6bf5
Romuald Brillout <github.com@brillout.com>
2018-07-23 20:56:27 +0200

    implement ORM integration

--- examples/typeorm/server/easyql/typeorm/EasyQLTypeORM.js
+++ examples/typeorm/server/easyql/typeorm/EasyQLTypeORM.js
@@ -14,18 +14,24 @@ function EasyQLTypeORM(easyql, typeormConfig) {
     easyql.QueryHandlers.push(queryHandler);
 
     const permissions = [];
-    const generatedSchemas = [];
+    const generatedEntities = [];
 
     return {
         addPermissions,
         closeConnection,
-        addModel: addModel.bind(null, generatedSchemas, connection),
+        addModel: addModel.bind(null, generatedEntities, connection),
     };
 
     async function queryHandler(params) {
         const {req, query, NEXT} = params;
         assert_internal(req && query && NEXT, params);
         if( ! connection ) {
+            const connectionOptions = Object.assign({}, typeormConfig);
+            connectionOptions.entities = (connectionOptions.entities||[]).slice();
+            console.log(generatedEntities);
+            // TODO
+            connectionOptions.entitySchemas = generatedEntities;
+
             connection = await createConnection(typeormConfig);
         }
         for(const permission of permissions) {
@@ -34,25 +40,25 @@ function EasyQLTypeORM(easyql, typeormConfig) {
             if( query.modelName === getModelName(permission.model) ) {
                 const {queryType} = query;
                 assert_usage(queryType, query);
-                const {model: schema} = permission;
-                assert_usage(schema, permission);
+                const {model: entity} = permission;
+                assert_usage(entity, permission);
                 if( queryType==='read' && await hasPermission(permission.read, params) ) {
-                    const objects = await connection.manager.find(schema);
+                    const objects = await connection.manager.find(entity);
                     return JSON.stringify({objects});
                 }
                 if( queryType==='write' && await hasPermission(permission.write, params) ) {
-                    const objects = await connection.manager.find(schema);
+                    const objects = await connection.manager.find(entity);
                     const objectProps = query.object;
                     assert_usage(objectProps, query);
 
                     /*
-                    const obj = new schema();
+                    const obj = new entity();
                     Object.assign(obj, objectProps);
                     await connection.manager.save(obj);
                     return JSON.stringify({objects: [obj]});
                     */
 
-                    const repository = connection.getRepository(schema);
+                    const repository = connection.getRepository(entity);
                     const obj = await repository.save(objectProps);
 
                     return JSON.stringify({objects: [obj]});
@@ -84,7 +90,7 @@ function EasyQLTypeORM(easyql, typeormConfig) {
     }
 }
 
-function addModel(generatedSchemas, connection, modelSpecFn) {
+function addModel(generatedEntities, connection, modelSpecFn) {
     assert_usage(connection===undefined);
 
     const types = {
@@ -100,20 +106,24 @@ function addModel(generatedSchemas, connection, modelSpecFn) {
     assert_usage(modelName);
     assert_usage(props && Object.entries(props).length>0);
 
-    const schemaObject = {
+    const entityObject = {
         name: modelName,
         columns: {},
     };
     Object.entries(props).forEach(([propName, propType]) => {
-        schemaObject.columns[propName] = getTypeormType(propType);
+        entityObject.columns[propName] = getTypeormType(propType);
     });
 
-    const schema = new EntitySchema(schemaObject);
-    assert_internal(schema.options.name===modelName);
+    //*
+    const entity = new EntitySchema(entityObject);
+    assert_internal(entity.options.name===modelName);
+    /*/
+    const entity = entityObject;
+    //*/
 
-    generatedSchemas.push(schema);
+    generatedEntities.push(entity);
 
-    return {model: schema};
+    return {model: entity};
 
     function getTypeormType(propType) {
         if( propType === types.ID ) {
