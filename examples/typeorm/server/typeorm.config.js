6e17555b64ef5e1effccb86988b88043c97a2a18
Romuald Brillout <github.com@brillout.com>
2018-07-23 14:20:28 +0200

    implement ORM integration

--- examples/typeorm/server/typeorm.config.js
+++ examples/typeorm/server/typeorm.config.js
@@ -1,10 +1,9 @@
-const requireAll = require('./utils/requireAll');
-console.log('af', require.bla);
+const {requireAll, requireContext} = require('./utils/requireAll');
+require.context = requireContext;
 
-console.log(2, require.context);
-const entities = requireAll(require.context("./models/entity", true, /\.ts$/));
-const migrations = requireAll(require.context("./models/migration", true, /\.ts$/));
-const subscribers = requireAll(require.context("./models/subscriber", true, /\.ts$/));
+const entities = requireAll(require.context("../models/entity", true, /\.ts$/));
+const migrations = requireAll(require.context("../models/migration", true, /\.ts$/));
+const subscribers = requireAll(require.context("../models/subscriber", true, /\.ts$/));
 
 module.exports = {
    type: "sqlite",
--- examples/typeorm/server/utils/requireAll.js
+++ examples/typeorm/server/utils/requireAll.js
@@ -1,34 +1,32 @@
 // TODO - make generic
-require.bla = 1;
-console.log('bef', require.bla);
 
 const pathModule = require('path');
 const fs = require('fs-extra');
 const assert_internal = require('reassert/internal');
 
-console.log(1);
-if( ! require.context ) {
-    require.context = (dirPathRelative, recursive, filterRegex) => {
-        const dirPathAbsolute = path.resolve(__dirname, '..', dirPathRelative);
+const requireContext = require.context || requireContextShim;
 
-        const modulePaths = fs.readdirSync(dirPathAbsolute);
-        const keys = () => modulePaths;
-        assert_internal(modulePaths.forEach(p => pathModule.isAbsolute(p)));
-
-        const loader = modulePath => require(modulePath);
-
-        loader.keys = keys;
-
-        return loader;
-    };
-}
-console.log(require.context);
-
-module.exports = requireAll;
+module.exports = {requireAll, requireContext};
 
 function requireAll(loader) {
     const modules = loader.keys().map(key => {
         return loader(key).default;
     });
     return modules;
+}
+
+function requireContextShim(dirPathRelative, recursive, filterRegex) {
+    const dirPathAbsolute = pathModule.resolve(__dirname, '..', dirPathRelative);
+
+    const modulePaths = (
+        fs.readdirSync(dirPathAbsolute)
+        .map(pathRelative => pathModule.resolve(dirPathAbsolute, pathRelative))
+    );
+    const keys = () => modulePaths;
+
+    const loader = modulePath => eval('require')(modulePath);
+
+    loader.keys = keys;
+
+    return loader;
 }
