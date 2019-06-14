f6e65ad669366689a98a8513b684e37142b1e01e
Romuald Brillout <github.com@brillout.com>
2019-06-14 11:31:22 +0200

    move code to ssr-coin

--- plugins/react-router/browserViewWrapper.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const {BrowserRouter} = require('react-router-dom');
-
-module.exports = browserViewWrapper;
-
-function browserViewWrapper(viewElement) {
-    return React.createElement(BrowserRouter, null, viewElement);
-}
-
