17ce7f83113a06e98ac7d5c93c362f7dacc31027
Romuald Brillout <github.com@brillout.com>
2019-07-30 21:10:50 +0200

    move code to ssr-coin [LAST-LEGACY-VERSION]

--- helpers/browser-config/example/config.js
+++
@@ -1,3 +0,0 @@
-const browswerConfig = require('..'); // npm install @brillout/browser-config
-
-browswerConfig.myNewConfig = 42;
--- helpers/browser-config/example/run.js
+++
@@ -1,6 +0,0 @@
-require('./config');
-
-const browswerConfig = require('..');
-
-// This will print 42
-console.log(browswerConfig.myNewConfig);
